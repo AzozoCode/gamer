385cd4f1da748c2f59ee99402fb1d8f91fd06947
joseph barrigah <josephbarrigah@gmail.com>
2025-03-09 12:39:43 +0000

    added a test to kickball function

--- soccer/main_test.go
+++ soccer/main_test.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_KickBall(t *testing.T) {
+	team := make([]FootballPlayer, 11)
+
+	for i := 0; i < len(team); i++ {
+		team[i] = FootballPlayer{
+			power:   rand.Intn(10) + 1,
+			stamina: rand.Intn(10) + 1,
+			name:    "test player",
+		}
+	}
+
+	for i := range team {
+		t.Run(team[i].name, func(t *testing.T) {
+
+			res := team[i].kickBall()
+			require.NotZero(t, res)
+			require.EqualValues(t, team[i].power+team[i].stamina, res)
+
+		})
+	}
+
+}
