b43dd1b84c5c7544d42ab3399f4c61254d2bcadc
joseph barrigah <josephbarrigah@gmail.com>
2025-03-09 00:57:34 +0000

    added an interface

--- soccer/main.go
+++ soccer/main.go
@@ -6,40 +6,64 @@ import (
 )
 
 type Player interface {
-	kickBall()
+	kickBall() int
+	getName() string
 }
 
 type FootballPlayer struct {
 	power   int
 	stamina int
+	name    string
 }
 
 type CR7 struct {
 	power   int
 	stamina int
 	SUI     int
+	name    string
 }
 
-func (c CR7) kickBall() {
-	shot := c.stamina + c.power*c.SUI
-	fmt.Printf("CR 7 is kicking the ball shot:%d\n", shot)
+type Messi struct {
+	power   int
+	stamina int
+	SUI     int
+	name    string
+}
+
+func (c CR7) kickBall() int {
+	return c.stamina + c.power*c.SUI
 
 }
 
-func (p FootballPlayer) kickBall() {
-	shot := p.stamina + p.power
+func (p CR7) getName() string {
+	return p.name
+}
 
-	fmt.Printf("I'm kicking the ball shot:%d\n", shot)
+func (m Messi) kickBall() int {
+	return m.stamina + m.power*m.SUI
+}
+
+func (p Messi) getName() string {
+	return p.name
+}
+
+func (p FootballPlayer) kickBall() int {
+	return p.stamina + p.power
+}
+
+func (p FootballPlayer) getName() string {
+	return p.name
 }
 
 func main() {
 
 	team := make([]Player, 11)
 
-	for i := 0; i < len(team)-1; i++ {
+	for i := 0; i < len(team)-2; i++ {
 		team[i] = FootballPlayer{
 			stamina: rand.Intn(10) + 1,
 			power:   rand.Intn(10) + 1,
+			name:    "Random",
 		}
 	}
 
@@ -47,10 +71,25 @@ func main() {
 		stamina: 10,
 		power:   10,
 		SUI:     10,
+		name:    "CR7",
 	}
 
-	for _, player := range team {
-		player.kickBall()
+	team[len(team)-2] = Messi{
+		stamina: 10,
+		power:   10,
+		SUI:     8,
+		name:    "Messi",
+	}
+
+	for i := range team {
+
+		fmt.Printf("%s is kicking the ball with shot: %d\n", team[i].getName(), team[i].kickBall())
+	}
+	fmt.Println("---------------------------------------------------------")
+
+	for _, p := range team {
+
+		fmt.Printf("%s is kicking the ball with shot: %d\n", p.getName(), p.kickBall())
 	}
 
 }
